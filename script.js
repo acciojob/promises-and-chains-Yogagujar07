fc79d5874a2e00dd0f234b4eb28f3b8805b706e1
Yogesh Patil <144556307+Yogagujar07@users.noreply.github.com>
2025-02-06 00:00:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+document.getElementById("voteForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    let name = document.getElementById("name").value.trim();
+    let age = document.getElementById("age").value.trim();
+    
+    if (!name || !age) {
+        alert("Please enter valid details.");
+        return;
+    }
+    
+    age = Number(age);
+    
+    let checkAge = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (age > 18) {
+                resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+                reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+        }, 4000);
+    });
+    
+    checkAge
+        .then(message => alert(message))
+        .catch(error => alert(error));
+});
